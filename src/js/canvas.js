3e9c596d967cde9e47f7e0c1a334199a7de435fc
BDunnCode <BDunnCode@github.com>
2023-11-01 17:27:21 -0500

    setting up. fixing broken dependencies.

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,178 @@
-import utils from './utils'
+import platform from './img/platform.png'
 
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = window.innerWidth 
+canvas.height = window.innerHeight
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.5;
+
+class Player {
+    constructor() {
+      this.position = {
+        x: 100, 
+        y: 100
+    }
+    this.velocity = {
+        x: 0,
+        y: 0
+    }
+
+    this.width = 30
+    this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+          this.velocity.y += gravity;
+        else this.velocity.y = 0;
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+// He used destructuring here.
+class platform {
+  constructor({x, y}) {
+    this.position = {
+      x,
+      y
+    }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+    this.width = 200
+    this.height = 20
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const player = new Player() 
+const platforms = [new platform({
+  x: 200, y: 100
+}), new platform({x: 500, y: 200})]
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+const keys = {
+    left: {
+    pressed: false
+  },
+    right: { 
+    pressed: false
   }
 }
 
-// Animation Loop
+let scrollOffset = 0;
+
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  platforms.forEach((platform) => { 
+    platform.draw()
+  })
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5
+  } else {
+    player.velocity.x = 0
+
+    if (keys.right.pressed) {
+      scrollOffset += 5
+      platforms.forEach((platform) => { 
+        platform.position.x -= 5
+      })   
+    } else if (keys.left.pressed) {
+      scrollOffset -= 5
+      platforms.forEach((platform) => { 
+        platform.position.x += 5
+    })       
+  }
 }
 
-init()
+console.log(scrollOffset)
+
+
+
+
+  // platform collision detection
+  platforms.forEach((platform) => {
+  if (
+    player.position.y + player.height <= platform.position.y && player.position.y + player.height
+    + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x 
+    && player.position.x <= platform.position.x + platform.width) {
+    player.velocity.y = 0;
+    }
+  })
+  if (scrollOffset > 2000) {
+    console.log('you win')
+  }
+}
+
+
+
 animate()
+
+window.addEventListener('keydown', ({ keyCode }) => {
+    console.log(keyCode);
+    switch(keyCode) {
+      case 65:
+        keys.left.pressed = true;
+        console.log('left')
+        break
+    
+      case 83:
+        console.log('down')
+        break
+
+      case 68:
+        console.log('right')
+        keys.right.pressed = true;
+        break
+
+      case 87:
+        console.log('up')
+        player.velocity.y -= 10
+        break
+    }
+})
+
+window.addEventListener('keyup', ({ keyCode }) => {
+    // console.log(keyCode);
+    switch(keyCode) {
+      case 65:
+        console.log('left')
+        keys.left.pressed = false;
+        break
+    
+      case 83:
+        console.log('down')
+        break
+
+      case 68:
+        console.log('right')
+        keys.right.pressed = false;
+        break
+
+      case 87:
+        console.log('up')
+        player.velocity.y -= 20
+        break
+    }
+})
+
+
+
+player.draw()
